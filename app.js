e2b2ccaf08939463fd96f510b92e5f911391be15
Elsayedcode <divemasry@live.nl>
2023-11-04 06:23:45 +0100

    node level1

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3004
+const mongoose = require('mongoose');
+
 
 app.get('/', (req, res) => {
   res.sendFile("./views/Home.html" , {root: __dirname});
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+
+mongoose.connect('mongodb+srv://divemasry:bzBsViLeKJQfbxiV@cluster0.i6j0m1y.mongodb.net/all-data?retryWrites=true&w=majority')
+.then(() => {
+  app.listen(port, () => {console.log(`http://localhost:${port}/`)});
 })
+.catch((error) => {console.error(error)});
+
