4c66aeef76bdf1f1182ac677a4201b697825f8f7
Elsayedcode <divemasry@live.nl>
2023-11-28 11:31:03 +0100

    latest update Node1

--- app.js
+++ app.js
@@ -1,17 +1,104 @@
-const express = require('express')
-const app = express()
-const port = 3004
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const port = 3001;
+const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+const User = require("./models/customerSchema");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+var moment = require('moment');
+var methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+// Auto refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+// GET Requst
+app.get("/", (req, res) => {
+  // result ==> array of objects
+  console.log("-------------------------------------");
+  User.find()
+    .then((result) => {
+      
+      res.render("index", { arr: result, moment:moment});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/Home.html" , {root: __dirname});
+app.get("/user/add.html", (req, res) => {
+  res.render("user/add");
+});
+
+app.get("/edit/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => { 
+  res.render("user/edit", {obj: result});
 })
+.catch((err) => {
+  console.log(err);
+});
+});
 
 
-mongoose.connect('mongodb+srv://divemasry:bzBsViLeKJQfbxiV@cluster0.i6j0m1y.mongodb.net/all-data?retryWrites=true&w=majority')
-.then(() => {
-  app.listen(port, () => {console.log(`http://localhost:${port}/`)});
-})
-.catch((error) => {console.error(error)});
+app.get("/view/:id", (req, res) => {
+  
+  // result ==> object
+  User.findById(req.params.id)
+    .then((result) => { 
+      res.render("user/view", {obj: result, moment:moment});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
+  
+});
+
+// POST Requst
+app.post("/user/add.html", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// Delete Requst
+app.delete("/edit/:id", (req, res) => {
+  User.findByIdAndDelete(req.params.id)
+  .then(() => {res.redirect("/")})
+  .catch((err) => {
+    console.log(err)})
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://divemasry:95ECCV.hRhUJrmx@cluster0.i6j0m1y.mongodb.net/all-data?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
